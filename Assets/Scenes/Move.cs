74c1dd3e212806becdd2e677424134aa45b7bb49
Приходько Владислав <3885vlad@gmail.com>
2024-06-11 17:09:06 +0300

    Added sprite flip

--- Assets/Scenes/Move.cs
+++ Assets/Scenes/Move.cs
@@ -1,18 +1,32 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Move : MonoBehaviour {
-	private float speed = 5;
-	private float jump = 6F;
+	float speed = 5;
+	float jump = 6F;
+	List<Collider2D> GroundColliders = new List<Collider2D>();
+	float x;
 
 	// Start is called before the first frame update
-	private void Start() {
-
+	void Start() {
+		x = transform.localScale.x;
 	}
 
 	// Update is called once per frame
-	private void Update() {
+	void Update() {
 		var moveDelta = Vector2.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 		transform.Translate(moveDelta);
+
+		if (moveDelta.x > 0) {
+			Vector3 v = transform.localScale;
+			v.x = x;
+			transform.localScale = v;
+		}
+		else if (moveDelta.x < 0) {
+			Vector3 v = transform.localScale;
+			v.x = -x;
+			transform.localScale = v;
+		}
 
 		if (Input.GetButtonDown("Jump") && IsGrounded()) {
 			GetComponent<Rigidbody2D>().AddForce(transform.up * jump, ForceMode2D.Impulse);
@@ -31,7 +45,7 @@ public class Move : MonoBehaviour {
 		}
 	}
 
-	private bool IsGrounded() {
+	bool IsGrounded() {
 		return true;
 	}
 }
